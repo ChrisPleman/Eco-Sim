557d11f4d0ceb4c4b93c14563dbabf936e56e9ee
Chris Pleman <chrispleman4@gmail.com>
2025-06-11 20:55:38 -0700

    Beginnings of a Genes mechanic
    I created the Gene class and IGenetic interface to map out the functionality of gene expression an inheritance in the simulation.
    
    At the moment, I'm thinking that each gene will have some specific trait associated with it, that will currently be represented by an Enum (probably not the best tbh). Additionally, it will contain some value that contains the "genetic data" for an organism to start of with, and/or pass on to its offspring.
    
    For inheritance, I figured implementing an IGenetic interface would be sufficient. This will allow both animals and plants to implement inheritance in the simulation.

---
+++ Assets/Assets/Gene.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Gene : MonoBehaviour
+{
+    public enum GeneTypes 
+    {
+        Gene1,
+        Gene2
+    }
+
+    public GeneTypes GeneType;
+
+    public float Value;
+}
--- Assets/Assets/Grow.cs
+++ Assets/Assets/Grow.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 [RequireComponent(typeof(Trees))]
 public class Grow : MonoBehaviour
 {
+    [Header("Various Traits")]
     [Range(0f, 1f)]
+    // todo: Probably should be inheritable traits as well
     public float GrowthSpeed;
+    public float FastestInitialGrowth;
+    public float GrowthDecaySpeed;
+    [Header("Cost of Growing")]
+    // todo: These two should likely be inheritable traits
+    public float InitialGrowthCost;
+    public float GrowthCostFactor;
     [Tooltip("Energy units per second it costs to grow.")]
+    // todo: More than just energy in the future
+    // todo: This should increase as the object grows
     public float GrowthCost;
+
+
+
+    // Private Variables //
+
     private Trees Tree;
     private float Scale;
 
+
     void Awake()
     {
-
+        // There is some initial cost
+        UpdateGrowthCost();
     }
 
     void Start()
     {
+        // todo: At some point this should be abstracted to any GrowableObject
         Tree = gameObject.GetComponent<Trees>();
     }
 
     void Update()
     {
-        if (Tree.Energy > 0f)
+        // We need at least as much as it costs to grow, well, to grow
+        if (Tree.Energy > GrowthCost)
         {
-            // Scale = DecayingGrowth(Tree.MaxHeight);
-            transform.localScale = new Vector3(Scale, Scale, Scale);
+            Scale += DecayingGrowth();
+            SetHeight(Scale, Tree.MaxHeight);
             Tree.Energy -= Time.deltaTime * GrowthCost;
+            UpdateGrowthCost();
         }
+    }
+
+    // todo: In this end this will cost several resources: energy, water, other(?)
+    private void UpdateGrowthCost()
+    {
+        GrowthCost = InitialGrowthCost * GrowthCostFactor * transform.localScale.x;
     }
 
     private void SetHeight(float scale, float cap)
@@ -37,10 +63,13 @@ public class Grow : MonoBehaviour
         transform.localScale = scaleVector;
     }
 
-    // private float DecayingGrowth(float scaler, float peak)
-    // {
-    //     return scaler / Mathf.Pow()
-    // }
+    private float DecayingGrowth()
+    {
+        // decaySpeed should be between 0 and 1, where the closer to zero you get, the slower it takes to decay
+        // fastestInitialGrowth is how fast we want the object to increase in scale at the onset
+        // ! Right now, we are just passing in single deltaTimes, when this should be a cumulative of time growing
+        return FastestInitialGrowth / Mathf.Pow(1 + GrowthDecaySpeed, Time.deltaTime);
+    }
 
     private Vector3 UniformVector3(float val)
     {
